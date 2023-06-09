7bdd795ef809ac97949b51aed26567ed8571be49
Patrick-Njiru <patricknjiru93@gmail.com>
2023-06-09 17:15:28 +0300

    api for the to-do list with react front-end.

---
+++ todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField(default=False)
+
+    'String representation of the class'
+    def __str__(self):
+        return self.title
